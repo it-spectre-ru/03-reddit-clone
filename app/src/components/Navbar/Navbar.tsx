7645c530394142f8a8a70d8ee0b03c8511db9efa
Avv <it.spectre.ru@gmail.com>
2023-03-06 17:27:04 +0500

    add <SearchInput />

--- app/src/components/Navbar/Navbar.tsx
+++ app/src/components/Navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 import { Flex, Image } from '@chakra-ui/react';
 import React from 'react';
+import SearchInput from './SearchInput';
 
 const Navbar: React.FC = () => {
   return (
@@ -8,6 +9,10 @@ const Navbar: React.FC = () => {
         <Image src="/images/redditFace.svg" height="30px" />
         <Image src="/images/redditText.svg" height="46px" display={{ base: 'none', md: 'unset' }} />
       </Flex>
+      <SearchInput />
+      {/* <Directory />
+      
+      <RightContent /> */}
     </Flex>
   );
 };
