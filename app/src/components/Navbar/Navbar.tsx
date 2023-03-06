737c98e86bb1f0c9f1669b94d251934702e829bd
Avv <it.spectre.ru@gmail.com>
2023-03-06 16:26:44 +0500

    add css styles

--- app/src/components/Navbar/Navbar.tsx
+++ app/src/components/Navbar/Navbar.tsx
@@ -2,6 +2,10 @@ import { Flex } from '@chakra-ui/react';
 import React from 'react';
 
 const Navbar: React.FC = () => {
-  return <Flex>Navbar</Flex>;
+  return (
+    <Flex bg="white" border="1px solid red">
+      Navbar
+    </Flex>
+  );
 };
 export default Navbar;
