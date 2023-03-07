bec004c8fdda9dac4025b3c3c6a255bec10f41a5
Avv <it.spectre.ru@gmail.com>
2023-03-07 15:33:13 +0500

    styling modal button

--- app/src/components/Modal/Login.tsx
+++ app/src/components/Modal/Login.tsx
@@ -20,9 +20,53 @@ const Login: React.FC<LoginProps> = () => {
 
   return (
     <form onSubmit={onSubmit}>
-      <Input name="email" placeholder="email" type="email" mb={2} onChange={onChange} />
-      <Input name="password" placeholder="password" type="password" onChange={onChange} />
-      <Button type="submit">Log In</Button>
+      <Input
+        required
+        name="email"
+        placeholder="email"
+        type="email"
+        mb={2}
+        onChange={onChange}
+        fontSize="10pt"
+        _placeholder={{ color: 'gray.500' }}
+        _hover={{
+          bg: 'white',
+          border: '1px solid',
+          bordeeColor: 'blue.500',
+        }}
+        _focus={{
+          outline: 'none',
+          bg: 'white',
+          border: '1px solid',
+          bordeeColor: 'blue.500',
+        }}
+        bg="gray.50"
+      />
+      <Input
+        required
+        name="password"
+        placeholder="password"
+        type="password"
+        onChange={onChange}
+        mb={2}
+        fontSize="10pt"
+        _placeholder={{ color: 'gray.500' }}
+        _hover={{
+          bg: 'white',
+          border: '1px solid',
+          bordeeColor: 'blue.500',
+        }}
+        _focus={{
+          outline: 'none',
+          bg: 'white',
+          border: '1px solid',
+          bordeeColor: 'blue.500',
+        }}
+        bg="gray.50"
+      />
+      <Button width="100%" height="36px" mt={2} mb={2} type="submit">
+        Log In
+      </Button>
     </form>
   );
 };
