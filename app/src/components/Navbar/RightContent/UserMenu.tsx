8a02499fe2015fd0520bc2824a576fa69116589c
Avv <it.spectre.ru@gmail.com>
2023-03-24 20:26:28 +0500

    setup Icon in Menu

--- app/src/components/Navbar/RightContent/UserMenu.tsx
+++ app/src/components/Navbar/RightContent/UserMenu.tsx
@@ -1,10 +1,21 @@
-import { ChevronDownIcon } from '@chakra-ui/icons';
-import { Menu, MenuButton, Button, MenuList, MenuItem, Icon, Flex } from '@chakra-ui/react';
-import { User } from 'firebase/auth';
 import React from 'react';
+import { ChevronDownIcon } from '@chakra-ui/icons';
+import {
+  Menu,
+  MenuButton,
+  Button,
+  MenuList,
+  MenuItem,
+  Icon,
+  Flex,
+  MenuDivider,
+} from '@chakra-ui/react';
+import { User } from 'firebase/auth';
 import { FaRedditSquare } from 'react-icons/fa';
 import { VscAccount } from 'react-icons/vsc';
 import { IoSparkles } from 'react-icons/io5';
+import { CgProfile } from 'react-icons/cg';
+import { MdOutlineLogin } from 'react-icons/md';
 
 type UserMenuProps = { user?: User | null };
 
@@ -15,26 +26,54 @@ const UserMenu: React.FC<UserMenuProps> = ({ user }) => {
         cursor="pointer"
         padding="0px 6px"
         borderRadius={4}
-        _hover={{ outline: '1px solid', outlineColor: 'gray.200' }}>
+        _hover={{
+          outline: '1px solid',
+          outlineColor: 'gray.200',
+        }}>
         {user ? (
           <Flex align="center">
             <Flex align="center">
               <>
-                <Icon fontSize={24} mr={1} color="gray.300" as={FaRedditSquare} />
+                <Icon
+                  fontSize={24}
+                  mr={1}
+                  color="gray.300"
+                  as={FaRedditSquare}
+                />
               </>
               <ChevronDownIcon />
             </Flex>
           </Flex>
         ) : (
-          <div>no user</div>
+          <Icon fontSize={24} color="gray.400" mr={1} as={VscAccount} />
         )}
       </MenuButton>
       <MenuList>
-        <MenuItem>Download</MenuItem>
-        <MenuItem>Create a Copy</MenuItem>
-        <MenuItem>Mark as Draft</MenuItem>
-        <MenuItem>Delete</MenuItem>
-        <MenuItem>Attend a Workshop</MenuItem>
+        <MenuItem
+          fontSize="10pt"
+          fontWeight={700}
+          _hover={{
+            bg: 'blue.500',
+            color: 'white',
+          }}>
+          <Flex align="center">
+            <Icon fontSize={20} mr={2} as={CgProfile} />
+            Profile
+          </Flex>
+        </MenuItem>
+        <MenuDivider />
+        <MenuItem
+          fontSize="10pt"
+          fontWeight={700}
+          _hover={{
+            bg: 'blue.500',
+            color: 'white',
+          }}>
+          <Flex align="center">
+            <Icon fontSize={20} mr={2} as={MdOutlineLogin} />
+            Log Out
+          </Flex>
+        </MenuItem>
       </MenuList>
     </Menu>
   );
