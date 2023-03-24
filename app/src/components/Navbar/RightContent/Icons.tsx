ec91710d5eae7a228feedb85e3d8d2a2162b0a3c
Avv <it.spectre.ru@gmail.com>
2023-03-24 19:33:56 +0500

    complete Icons

--- app/src/components/Navbar/RightContent/Icons.tsx
+++ app/src/components/Navbar/RightContent/Icons.tsx
@@ -48,7 +48,7 @@ const Icons: React.FC = () => {
           cursor="pointer"
           borderRadius={4}
           _hover={{ bg: 'gray.200' }}>
-          <Icon as={BsChatDots} fontSize={22} />
+          <Icon as={BsChatDots} fontSize={20} />
         </Flex>
         <Flex
           mr={1.5}
@@ -57,16 +57,17 @@ const Icons: React.FC = () => {
           cursor="pointer"
           borderRadius={4}
           _hover={{ bg: 'gray.200' }}>
-          <Icon as={IoNotificationsOutline} fontSize={22} />
+          <Icon as={IoNotificationsOutline} fontSize={20} />
         </Flex>
         <Flex
+          display={{ base: 'none', md: 'flex' }}
           mr={1.5}
           ml={1.5}
           padding={1}
           cursor="pointer"
           borderRadius={4}
           _hover={{ bg: 'gray.200' }}>
-          <Icon as={GrAdd} fontSize={22} />
+          <Icon as={GrAdd} fontSize={20} />
         </Flex>
       </>
     </Flex>
