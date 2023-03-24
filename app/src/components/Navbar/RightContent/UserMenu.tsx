955389d4d04443fdca741db884ba4d84c477d8ba
Avv <it.spectre.ru@gmail.com>
2023-03-24 19:51:28 +0500

    styling Menu button

--- app/src/components/Navbar/RightContent/UserMenu.tsx
+++ app/src/components/Navbar/RightContent/UserMenu.tsx
@@ -1,14 +1,34 @@
 import { ChevronDownIcon } from '@chakra-ui/icons';
-import { Menu, MenuButton, Button, MenuList, MenuItem } from '@chakra-ui/react';
+import { Menu, MenuButton, Button, MenuList, MenuItem, Icon, Flex } from '@chakra-ui/react';
 import { User } from 'firebase/auth';
 import React from 'react';
+import { FaRedditSquare } from 'react-icons/fa';
+import { VscAccount } from 'react-icons/vsc';
+import { IoSparkles } from 'react-icons/io5';
 
 type UserMenuProps = { user?: User | null };
 
 const UserMenu: React.FC<UserMenuProps> = ({ user }) => {
   return (
     <Menu>
-      <MenuButton>{user ? <div>user</div> : <div>no user</div>}</MenuButton>
+      <MenuButton
+        cursor="pointer"
+        padding="0px 6px"
+        borderRadius={4}
+        _hover={{ outline: '1px solid', outlineColor: 'gray.200' }}>
+        {user ? (
+          <Flex align="center">
+            <Flex align="center">
+              <>
+                <Icon fontSize={24} mr={1} color="gray.300" as={FaRedditSquare} />
+              </>
+              <ChevronDownIcon />
+            </Flex>
+          </Flex>
+        ) : (
+          <div>no user</div>
+        )}
+      </MenuButton>
       <MenuList>
         <MenuItem>Download</MenuItem>
         <MenuItem>Create a Copy</MenuItem>
