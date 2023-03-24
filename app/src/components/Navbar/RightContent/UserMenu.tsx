a57004a615462bd7e7fecfed795a9b8ccee9e241
Avv <it.spectre.ru@gmail.com>
2023-03-24 19:42:29 +0500

    view user text on User login

--- app/src/components/Navbar/RightContent/UserMenu.tsx
+++ app/src/components/Navbar/RightContent/UserMenu.tsx
@@ -1,15 +1,14 @@
 import { ChevronDownIcon } from '@chakra-ui/icons';
 import { Menu, MenuButton, Button, MenuList, MenuItem } from '@chakra-ui/react';
+import { User } from 'firebase/auth';
 import React from 'react';
 
-type UserMenuProps = {};
+type UserMenuProps = { user?: User | null };
 
-const UserMenu: React.FC<UserMenuProps> = () => {
+const UserMenu: React.FC<UserMenuProps> = ({ user }) => {
   return (
     <Menu>
-      <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
-        Actions
-      </MenuButton>
+      <MenuButton>{user ? <div>user</div> : <div>no user</div>}</MenuButton>
       <MenuList>
         <MenuItem>Download</MenuItem>
         <MenuItem>Create a Copy</MenuItem>
