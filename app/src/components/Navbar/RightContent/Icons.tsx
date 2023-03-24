c67e4e280650791f368312c561402e4f06fd5db4
Avv <it.spectre.ru@gmail.com>
2023-03-24 17:45:53 +0500

    add icon on Icon component

--- app/src/components/Navbar/RightContent/Icons.tsx
+++ app/src/components/Navbar/RightContent/Icons.tsx
@@ -1,4 +1,4 @@
-import { Flex } from '@chakra-ui/react';
+import { Flex, Icon } from '@chakra-ui/react';
 import React from 'react';
 import { BsArrowUpRightCircle, BsChatDots } from 'react-icons/bs';
 import { GrAdd } from 'react-icons/gr';
@@ -7,8 +7,68 @@ import { IoFilterCircleOutline, IoNotificationsOutline, IoVideocamOutline } from
 const Icons: React.FC = () => {
   return (
     <Flex>
-      <Flex>icons</Flex>
-      <></>
+      <Flex
+        display={{ base: 'none', md: 'flex' }}
+        align="center"
+        borderRight="1px solid"
+        borderColor="gray.200">
+        <Flex
+          mr={1.5}
+          ml={1.5}
+          padding={1}
+          cursor="pointer"
+          borderRadius={4}
+          _hover={{ bg: 'gray.200' }}>
+          <Icon as={BsArrowUpRightCircle} fontSize={20} />
+        </Flex>
+        <Flex
+          mr={1.5}
+          ml={1.5}
+          padding={1}
+          cursor="pointer"
+          borderRadius={4}
+          _hover={{ bg: 'gray.200' }}>
+          <Icon as={IoFilterCircleOutline} fontSize={22} />
+        </Flex>
+        <Flex
+          mr={1.5}
+          ml={1.5}
+          padding={1}
+          cursor="pointer"
+          borderRadius={4}
+          _hover={{ bg: 'gray.200' }}>
+          <Icon as={IoVideocamOutline} fontSize={22} />
+        </Flex>
+      </Flex>
+      <>
+        <Flex
+          mr={1.5}
+          ml={1.5}
+          padding={1}
+          cursor="pointer"
+          borderRadius={4}
+          _hover={{ bg: 'gray.200' }}>
+          <Icon as={BsChatDots} fontSize={22} />
+        </Flex>
+        <Flex
+          mr={1.5}
+          ml={1.5}
+          padding={1}
+          cursor="pointer"
+          borderRadius={4}
+          _hover={{ bg: 'gray.200' }}>
+          <Icon as={IoNotificationsOutline} fontSize={22} />
+        </Flex>
+        <Flex
+          mr={1.5}
+          ml={1.5}
+          padding={1}
+          cursor="pointer"
+          borderRadius={4}
+          _hover={{ bg: 'gray.200' }}>
+          <Icon as={GrAdd} fontSize={22} />
+        </Flex>
+      </>
     </Flex>
   );
 };
