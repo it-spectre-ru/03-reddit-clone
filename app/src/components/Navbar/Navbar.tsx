dfef92f4b9a609619a19bd7ee804a5ef8eb13eb3
Avv <it.spectre.ru@gmail.com>
2023-03-27 08:59:42 +0500

    setup comunity menu dropdown

--- app/src/components/Navbar/Navbar.tsx
+++ app/src/components/Navbar/Navbar.tsx
@@ -2,6 +2,7 @@ import { auth } from '@/src/firebase/clientApp';
 import { Flex, Image } from '@chakra-ui/react';
 import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import Directory from './Directory/Directory';
 import RightContent from './RightContent/RightContent';
 import SearchInput from './SearchInput';
 
@@ -11,11 +12,15 @@ const Navbar: React.FC = () => {
     <Flex bg="white" height="44px" padding="6px 12px">
       <Flex align="center">
         <Image src="/images/redditFace.svg" height="30px" />
-        <Image src="/images/redditText.svg" height="46px" display={{ base: 'none', md: 'unset' }} />
+        <Image
+          src="/images/redditText.svg"
+          height="46px"
+          display={{ base: 'none', md: 'unset' }}
+        />
       </Flex>
+      <Directory />
       <SearchInput />
       <RightContent user={user} />
-      {/* <Directory />*/}
     </Flex>
   );
 };
