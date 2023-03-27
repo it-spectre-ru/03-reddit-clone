777cc42ec0ce231d53234580e3d5d82c0d332dd9
Avv <it.spectre.ru@gmail.com>
2023-03-27 16:57:33 +0500

    styling directory menu

--- app/src/components/Navbar/Directory/Directory.tsx
+++ app/src/components/Navbar/Directory/Directory.tsx
@@ -1,6 +1,7 @@
 import { ChevronDownIcon } from '@chakra-ui/icons';
-import { Flex, Menu, MenuButton, MenuList } from '@chakra-ui/react';
+import { Flex, Icon, Menu, MenuButton, MenuList, Text } from '@chakra-ui/react';
 import React from 'react';
+import { TiHome } from 'react-icons/ti';
 
 const UserMenu: React.FC = () => {
   return (
@@ -9,12 +10,21 @@ const UserMenu: React.FC = () => {
         cursor="pointer"
         padding="0px 6px"
         borderRadius={4}
+        mr={2}
+        ml={{ base: 0, md: 2 }}
         _hover={{
           outline: '1px solid',
           outlineColor: 'gray.200',
         }}>
         <Flex align="center">
-          <Flex align="center"></Flex>
+          <Flex align="center">
+            <Icon fontSize={24} mr={{ base: 1, md: 2 }} as={TiHome} />
+            <Flex display={{ base: 'none', lg: 'flex' }}>
+              <Text fontWeight={600} fontSize="10pt">
+                Home
+              </Text>
+            </Flex>
+          </Flex>
           <ChevronDownIcon />
         </Flex>
       </MenuButton>
